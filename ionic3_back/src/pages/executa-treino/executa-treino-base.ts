7c482f578dac08736f63f668c953d325594f9029
Paulo <paulofore@gmail.com>
2019-04-26 21:54:56 -0300

    evolui

--- ionic3_back/src/pages/executa-treino/executa-treino-base.ts
+++ ionic3_back/src/pages/executa-treino/executa-treino-base.ts
@@ -1,74 +1,75 @@
-import { DiaTreino, ExecucaoItemSerie, DiaTreinoApi, ExecucaoItemSerieApi, LoopBackFilter } from '../../shared/sdk';
+import { DiaTreino, ExecucaoItemSerie,  DiaTreinoApi , ExecucaoItemSerieApi, LoopBackFilter } from '../../shared/sdk';
 import { NavParams, NavController } from 'ionic-angular';
 
 // Tipo: GETPUT
 export abstract class ExecutaTreinoPageBase {
-
-	protected item: DiaTreino;
-
-	// filtro com parametro id
-	protected abstract filtroLoadId(id: any): LoopBackFilter;
-	// filtro sem parametro id
-	protected abstract filtroLoadOne(): LoopBackFilter;
-	// chamada depois do submit e de nova inicializacao
-	protected abstract resultadoSubmit(result: ExecucaoItemSerie)
-
-	constructor(public navParams: NavParams,
-		public navCtrl: NavController,
-		public srv: DiaTreinoApi,
-		public srvPut: ExecucaoItemSerieApi) {
-	}
-
+  
+  protected item: DiaTreino;
+  
+  // filtro com parametro id
+  protected abstract filtroLoadId(id:any) : LoopBackFilter;
+  // filtro sem parametro id
+  protected abstract filtroLoadOne() : LoopBackFilter;
+  // chamada depois do submit e de nova inicializacao
+  protected abstract resultadoSubmit(result:ExecucaoItemSerie)
+ 
+  constructor(	public navParams: NavParams,
+  				public navCtrl: NavController,
+				public srv: DiaTreinoApi, 
+				public srvPut: ExecucaoItemSerieApi) {
+  } 
+  
 	private inicializaItem() {
 		this.item = this.navParams.get('item');
 		console.log('ExecutaTreinoPageBase:ItemParametro: ', this.item);
 		if (!this.item) {
 			var id = this.navParams.get('id');
-			console.log('ExecutaTreinoPageBase:Id: ', id);
+			console.log('ExecutaTreinoPageBase:Id: ' , id);
 			if (id) {
-				console.log('ExecutaTreinoPageBase:filtro: ', JSON.stringify(this.filtroLoadId(id)));
+				console.log('ExecutaTreinoPageBase:filtro: ' , JSON.stringify(this.filtroLoadId(id)));
 				console.log('DiaTreino.findById');
 				this.srv.findById(id, this.filtroLoadId(id))
 					.subscribe(
 						(result: DiaTreino) => {
 							this.item = result;
-							console.log('ExecutaTreinoPageBase.item: ', this.item)
+							console.log('ExecutaTreinoPageBase.item: ' , this.item)
 						},
-						(erro: any) => console.log('ExecutaTreinoPageBase:LoadId(Erro): ', JSON.stringify(erro))
+						(erro: any) => console.log('ExecutaTreinoPageBase:LoadId(Erro): ' , JSON.stringify(erro))
 					)
-			} else {
-				console.log('ExecutaTreinoPageBase:filtro: ', JSON.stringify(this.filtroLoadOne()));
+			} else  {
+				console.log('ExecutaTreinoPageBase:filtro: ' , JSON.stringify(this.filtroLoadOne()));
 				console.log('DiaTreino.findOne');
 				this.srv.findOne(this.filtroLoadOne())
 					.subscribe(
 						(result: DiaTreino) => {
 							this.item = result;
-							console.log('ExecutaTreinoPageBase.item: ', this.item)
+							console.log('ExecutaTreinoPageBase.item: ' , this.item)
 						},
-						(erro: any) => console.log('ExecutaTreinoPageBase:LoadId(Erro): ', JSON.stringify(erro))
+						(erro: any) => console.log('ExecutaTreinoPageBase:LoadId(Erro): ' , JSON.stringify(erro))
 					)
 			}
-		}
+		}  
 	}
 
-
-	ionViewWillEnter() {
-		console.log('ionViewWillEnter ExecutaTreinoPage<<GETPUT>>');
-		this.inicializaItem();
-	}
-	ionViewDidLoad() {
-		console.log('ionViewDidLoad ExecutaTreinoPage<<GETPUT>>');
-	}
-
-
-	protected submit(item: ExecucaoItemSerie) {
-		console.log('ExecutaTreinoPageBase:Submit-Item:', this.item);
-		this.srvPut.submitExecutaTreinoPage(this.item)
-			.subscribe((resultado: ExecucaoItemSerie) => {
-				console.log('ExecutaTreinoPageBase:Submit-Result: ', resultado);
+  
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter ExecutaTreinoPage<<GETPUT>>');
+    this.inicializaItem();
+  }
+  ionViewDidLoad() {
+  	console.log('ionViewDidLoad ExecutaTreinoPage<<GETPUT>>');
+  }
+  
+  
+	protected submit(itemSubmit:ExecucaoItemSerie ) {
+		console.log('ExecutaTreinoPageBase:Submit-Item:' , itemSubmit);
+		this.srvPut.submitExecutaTreinoPage(itemSubmit)
+			.subscribe((resultado:ExecucaoItemSerie) => {
+				console.log('ExecutaTreinoPageBase:Submit-Result: ' , resultado);
 				this.resultadoSubmit(resultado);
 				this.inicializaItem();
 			})
 	}
 }
-
+    
+    
